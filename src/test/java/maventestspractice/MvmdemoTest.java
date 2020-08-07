5fe206b823a05ba415c9d0f137d56f53500464b3
pra500 <pragtig6@gmail.com>
2020-08-07 13:19:38 +0530

    old project

---
+++ src/test/java/maventestspractice/MvmdemoTest.java
@@ -0,0 +1,48 @@
+package maventestspractice;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MvmdemoTest {
+	
+	@Test(priority = 1)
+	public void addTest()
+	{
+		System.out.println("add");
+		int a=12;
+		int b=13;
+		Assert.assertEquals(25, a+b);
+		
+	}
+	
+	
+	@Test(priority = 2)
+	public void subTest()
+	{
+		System.out.println("sub");
+		int a=12;
+		int b=13;
+		Assert.assertEquals(1, b-a);
+		
+	}
+	
+	
+
+	@Test(priority = 3)
+	public void mulTest()
+	{
+		System.out.println("mul");
+		int a=12;
+		int b=13;
+		Assert.assertEquals(156, a*b);
+		
+	}
+	
+	
+
+	
+	
+	
+	
+	
+}
